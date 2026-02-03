1fa9b0f2fe203408c0779b28a0c1b0e327ef8cfc
Admin <alenapachkovskay@gmail.com>
2026-02-03 16:55:29 +0700

    feat: labaratory work 3

--- js/main.js
+++ js/main.js
@@ -8,6 +8,6 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         inventory:100,
-        
+        onSale: true,
     }
 })
