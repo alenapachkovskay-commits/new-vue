d995e25f392793648d2b2751d9621b3e79b12294
Admin <alenapachkovskay@gmail.com>
2026-02-03 18:30:43 +0700

    feat: labaratory work 5. fix cart negative score

--- js/main.js
+++ js/main.js
@@ -29,8 +29,10 @@ let app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        deleteFromCart(){
-            this.cart -= 1
+        deleteFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
         },
         updateProduct(variantImage) {
             this.image = variantImage
